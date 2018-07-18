5ff4bfc4bcc07e7b801b432c4fc44311523386d9
Bogdan Nedelcu <nedbog@gmail.com>
2018-07-18 12:03:12 +0300

    add ExtensionMethods project

---
+++ AdvancedCSharpMoshHamedani/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 4, 5, 7, 10};
+            Console.WriteLine(numbers.Max());
+
+            Console.WriteLine(shortenedPost);
+
+            Console.ReadLine();
+        }
+    }
+}
